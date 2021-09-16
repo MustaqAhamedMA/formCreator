8563cab7db005a49bb4799df4dfc0668f2f8904c
Mohammed ismail <87133585+ismail-anywhere@users.noreply.github.com>
2021-09-16 13:49:25 +0530

    Update InputDetails.js

--- src/Components/InputDetails.js
+++ src/Components/InputDetails.js
@@ -1,23 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 
 function InputDetails(props) {
-    const [inputFieldDetails , setInputFieldDetails] = useState({})
+    const [inputFieldDetails , setInputFieldDetails] = useState(props.inputFieldDetails)
+    useEffect(() => {
+        setInputFieldDetails(props.inputFieldDetails);
+    }, [props.inputFieldDetails])
     return (
         <div className="m-3 p-3 border border-dark rounded">
             {Object.keys(props.inputFieldDetails).map(key=>{
-                if(key !== 'options'){
-                    return <div class="form-group">
-                                <label for="exampleInputEmail1">{key}</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder={key}/>
-                            </div>
-                }else{
-                    return <div class="form-group">
-                                <label for="exampleInputEmail1">{key}</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder={key}/>
-                            </div>
+                if(key !== 'type'){
+                    if(key !== 'options'){
+                        return <div className="form-group" key={key}>
+                                    <label htmlFor="exampleInputEmail1">{key}</label>
+                                    <input type="text" className="form-control" id="exampleInputEmail1" placeholder={key} onChange={(text)=>{
+                                        Object.keys(inputFieldDetails).map(objkey=>{
+                                            if(objkey === key){
+                                                inputFieldDetails[key] = text.target.value
+                                            }
+                                        })
+                                    }}/>
+                                </div>
+                    }else{
+                        return <div className="form-group" key={key}>
+                                    <label htmlFor="exampleInputEmail1">{key}</label>
+                                    <input type="text" className="form-control" id="exampleInputEmail1" placeholder={key}/>
+                                </div>
+                    }
                 }
             })}
-            <button className="btn btn-info" onClick={()=>{props.InputDetailsSubmit()}}>Submit</button>
+            <button className="btn btn-info" onClick={()=>{
+                props.InputDetailsSubmit(inputFieldDetails)
+                }}>Submit</button>
         </div>
     )
 }
