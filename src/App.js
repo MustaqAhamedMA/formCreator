a883b495043e6bdaf4e40d1f46c410a96e463e18
Mohammed ismail <87133585+ismail-anywhere@users.noreply.github.com>
2021-09-16 13:48:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,74 +1,75 @@
 import {useState} from 'react'
 import ButtonTypes from './Components/ButtonTypes'
 import InputDetails from './Components/InputDetails';
+import MainInput from './Components/MainInput';
 
 
 
 function App() {
   const [buttons, setButtons] = useState(['text','email','password','date','time','checkbox','dropdown','radio']);
   const [inputFieldDetails, setInputFieldDetails] = useState({})
-  const [type , setType] = useState('')
   const [inputFieldDetailsView , setInputFieldDetailsView] = useState(false)
+  const [inputDetails , setInputDetails] = useState([])
 
   const buttonOnClick = (type) => {
     switch (type) {
       case 'text':
-        setType(type)
         setInputFieldDetailsView(true)
         setInputFieldDetails({
+          type: 'text',
           label : '',
           placeholder : ''
         })
         break;
       case 'password':
-        setType(type)
         setInputFieldDetailsView(true)
         setInputFieldDetails({
+          type:'password',
           label : '',
           placeholder : ''
         })
         break;
       case 'email':
-        setType(type)
         setInputFieldDetailsView(true)
         setInputFieldDetails({
+          type : 'email',
           label : '',
           placeholder : ''
         })
         break;
       case 'date':
-        setType(type)
         setInputFieldDetailsView(true)
         setInputFieldDetails({
+          type: 'date',
           label : ''
         })
         break;
       case 'time':
-        setType(type)
         setInputFieldDetailsView(true)
         setInputFieldDetails({
+          type : 'time',
           label : '',
         })
         break;
       case 'checkbox':
-        setType(type)
         setInputFieldDetailsView(true)
         setInputFieldDetails({
+          type : 'checkbox',
           label : '',
         })
         break;
       case 'dropdown':
-        setType(type)
         setInputFieldDetailsView(true)
         setInputFieldDetails({
+          type : 'dropdown',
           label : '',
           options : []
         })
         break;
       case 'radio':
-        setType(type)
         setInputFieldDetailsView(true)
         setInputFieldDetails({
+          type : 'radio',
           label : '',
           group : '',
           options:[]
@@ -80,14 +81,17 @@ function App() {
     }
   }
 
-  const InputDetailsSubmit = () => {
+  const InputDetailsSubmit = (data) => {
     setInputFieldDetailsView(false)
+    setInputDetails([...inputDetails,data])
   }
   return (
     <div className="container">
       <ButtonTypes buttons = {buttons} buttonOnClick={buttonOnClick}/>
       <div className="row">
-        <div className="col-md-8"></div>
+        <div className="col-md-8">
+          <MainInput inputDetails={inputDetails}/>
+        </div>
         <div className="col-md-4">
           {inputFieldDetailsView && <InputDetails inputFieldDetails={inputFieldDetails} InputDetailsSubmit={InputDetailsSubmit}/>}
         </div>
